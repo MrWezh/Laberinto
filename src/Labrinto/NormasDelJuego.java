5e50c938a18f2b6be5fdc28124119fccda5ad33a
Wenkai <zhouwenkai16@gmail.com>
2024-04-22 12:50:57 +0200

    Menu de reglas creada, pero falta añadir las reglas

---
+++ src/Labrinto/NormasDelJuego.java
@@ -0,0 +1,23 @@
+package Labrinto;
+
+public class NormasDelJuego {
+
+    public void menuReclas(){
+
+    }
+
+    public void general(){
+
+    }
+
+    public void controles(){
+
+    }
+
+    public void combate(){
+
+    }
+
+    
+    
+}
